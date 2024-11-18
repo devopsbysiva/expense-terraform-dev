18c196c5ec7d3eb645ef701052701ed4f9b5b714
ganeshmoka <ganeshmoka1999@gmail.com>
2024-11-18 21:02:08 +0530

    vpcand sg

---
+++ 20-sg/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~>5.0"
+    }   
+}
+
+# backend "s3" {
+#     bucket         = "myorg-terraform-states"
+#     key            = "myapp/production/tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "TableName"
+#   }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
