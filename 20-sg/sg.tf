74634f51b6d2545a9ff2aa2108524c580bfa105b
ganeshmoka <ganeshmoka1999@gmail.com>
2024-11-21 05:16:59 +0530

    dev module

--- 20-sg/sg.tf
+++ 20-sg/sg.tf
@@ -1,7 +1,23 @@
-module "aws_sg" {
-    source = "../terraform-aws-security-group"
+module "mysql_sg" {
+    source = "../../terraform-aws-security-group"
     project_name = var.project_name
     environment = var.environment
     vpc_id = data.aws_ssm_parameter.vpc_id.value
-    sg_name = var.sg_name   
+    sg_name = "mysql"   
+}
+
+module "backend_sg" {
+    source = "../../terraform-aws-security-group"
+    project_name = var.project_name
+    environment = var.environment
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    sg_name = "backend"   
+}
+
+module "frontend_sg" {
+    source = "../../terraform-aws-security-group"
+    project_name = var.project_name
+    environment = var.environment
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    sg_name = "frontend"   
 }
