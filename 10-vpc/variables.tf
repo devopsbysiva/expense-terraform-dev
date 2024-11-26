c4e61b89cc18abe954a1461c4cf2c6e445502423
ganeshmoka <ganeshmoka1999@gmail.com>
2024-11-26 21:30:46 +0530

    vpc

--- 10-vpc/variables.tf
+++ 10-vpc/variables.tf
@@ -6,15 +6,15 @@ variable "envirnoment" {
     default = "dev"
 }
 
-variable "public_subnet_cidr" {
+variable "public_subnet_cidrs" {
     default = ["10.0.1.0/24","10.0.2.0/24"]
 }
 
-variable "private_subnet_cidr" {
+variable "private_subnet_cidrs" {
     default = ["10.0.11.0/24","10.0.12.0/24"]
 }
 
-variable "database_subnet_cidr" {
+variable "database_subnet_cidrs" {
     default = ["10.0.21.0/24","10.0.22.0/24"]
 }
 
--- 10-vpc/vpc.tf
+++ 10-vpc/vpc.tf
@@ -2,9 +2,9 @@ module "aws_vpc" {
     source = "../../terraform_aws_vpc"
     project = var.project_name
     envirnoment = var.envirnoment
-    public_subnet_cidr = var.public_subnet_cidr
-    private_subnet_cidr =  var.private_subnet_cidr
-    database_subnet_cidr = var.database_subnet_cidr
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs =  var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
     is_peering_requried = true 
  
 }
