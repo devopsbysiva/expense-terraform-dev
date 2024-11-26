4838d537056cf1cab1cce303a30d8c9bcc64340b
ganeshmoka <ganeshmoka1999@gmail.com>
2024-11-26 22:30:13 +0530

    vpc

---
+++ 40-apps/providers.tf
@@ -0,0 +1,41 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~>5.0"
+        
+    }  
+}
+backend "s3" {
+    bucket         = "myorg-terraform-states"
+    key            = "expense_dev_bastion"
+    region         = "us-east-1"
+    dynamodb_table = "TableName"
+
+}
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+
+# terraform {
+#     required_providers {
+#       aws = {
+#         source = "hashicorp/aws"
+#         version = "~>5.0"
+#     }   
+# }
+
+# # backend "s3" {
+# #     bucket         = "myorg-terraform-states"
+# #     key            = "expense_dev_bastion"
+# #     region         = "us-east-1"
+# #     dynamodb_table = "TableName"
+# #   }
+# }
+
+# provider "aws" {
+#     region = "us-east-1"
+# }
---
+++ 40-apps/variables.tf
@@ -0,0 +1,33 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "mysql_tags" {
+    default = {
+        component = "mysql"
+    }
+}
+
+variable "backend_tags" {
+    default = {
+        component = "backend"
+    }
+}
+
+variable "frontend_tags" {
+    default = {
+        component = "frontend"
+    }
+}
